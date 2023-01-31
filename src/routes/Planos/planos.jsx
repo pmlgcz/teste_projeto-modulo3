b88657b72c7df0b7f482ce0d69d46c1fe1c76207
Pamela Cruz <117556345+pmlgcz@users.noreply.github.com>
2023-01-31 16:54:30 -0300

    Update planos.jsx

--- src/routes/Planos/planos.jsx
+++ src/routes/Planos/planos.jsx
@@ -2,27 +2,27 @@ import "./Planos.css"
 import { useEffect } from 'react';
 import { useState } from 'react';
 import API from '../../axios/config'
-import EmCartazIMG from '../../images/cinema.png'
-import Cinemas from "../Catalogo/catalogo";
+import PlanosIMG from '../../images/play.png'
+import Catalogo from "../Catalogo/catalogo";
 
-export default function Precos() {
+export default function Planos() {
 
 
     const [precos, setPreco] = useState([]);
 
     useEffect(() => {
-        API.get("/planos").then((response) => setPreco(response.data));
+        API.get("/planos").then((response) => setPlanos(response.data));
     }, []);
 
 
     return (
         <>
         <div id="Planos">
-        <br></br><h2><img src={EmCartazIMG}/> Preços</h2><br></br>
+        <br></br><h2><img src={PlanosIMG}/> Planos</h2><br></br>
         </div>
         <ul className="lista-planos">
             <li>
-                {precos.map(function valores(item){
+                {planos.map(function valores(item){
                     return <div>
                         <p><b> </b> {item. }</p>
                         <p><b> </b> {item. }</p>
