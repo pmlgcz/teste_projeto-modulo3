68b2b7bd36124d0708905015a3e8d4ddcb875239
Pamela Cruz <117556345+pmlgcz@users.noreply.github.com>
2023-01-31 19:18:14 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
 import instagram from "../images/instagram.png"
 import facebook from "../images/facebook.png"
 import twitter from "../images/twitter.png"
-import googleplus from "../images/google-plus.png" 
+
 import "./Footer.css"
 export default function Footer () {
 
     return (
         <div className="Footer">
        
-        <p> SeriesFlix é um site de series feito para o modulo 3 (REACT) do curso Programadores Cariocas, cujo objetivo consumir fake API e utilizar os métodos HTTP</p>
+        <p> SeriesFlix é um site de series feito para o modulo 3 (REACT) do curso Programadores Cariocas, cujo objetivo  consumir fake API</p>
         <ul className="listaFooter">
             <li><img className="imgFooter" src={instagram} alt="" /></li>
             <li><img className="imgFooter" src={facebook} alt="" /> </li>
