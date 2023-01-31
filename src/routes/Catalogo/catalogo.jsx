a72bc2aa58676c83292359d1df0ac684b9433a2d
Pamela Cruz <117556345+pmlgcz@users.noreply.github.com>
2023-01-31 17:05:37 -0300

    Update catalogo.jsx

--- src/routes/Catalogo/catalogo.jsx
+++ src/routes/Catalogo/catalogo.jsx
@@ -1,28 +1,28 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
 import API from '../../axios/config'
-import EmCartazIMG from '../../images/cinema.png'
+import MaisVistasIMG from '../../images/play.png'
 import "./Cinemas.css"
-export default function Cinemas() {
+export default function Catalogo() {
 
-  const [cinema, setCinema] = useState([]);
+  const [catalogo, setCatalogo] = useState([]);
 
   useEffect(() => {
-    API.get("/cinemas").then((response) => setCinema(response.data));
+    API.get("/catalogo").then((response) => setCatalogo(response.data));
   }, []);
 
   return (
     <>
-      <br></br><div id="Cinemas">
-        <h2> <img src={EmCartazIMG}/> nossos cinemas</h2><br></br>
+      <br></br><div id="Catalogo">
+        <h2> <img src={MaisVistasIMG}/>Series mais assistidas</h2><br></br>
       </div>
-      <ul className='lista-cinemas'>
+      <ul className='lista-catalogo'>
         <li>
-          {cinema.map(function locais(item) {
+          {catalogo.map(function locais(item) {
             return <div>
-                    <p>Cinema {item.id}</p>
-                    <p><strong>Local:</strong> {item.local}</p>
-                    <p><strong>Estado:</strong> {item.estado}</p>
+                    <p>Catalogo {item.id}</p>
+
+
                   </div>;
           })}
         </li>
